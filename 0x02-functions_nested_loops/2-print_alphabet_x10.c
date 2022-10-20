d054cfad2093ee745654e9b6a76514ff213d1e85
Sotayo Seun <sotayomatthews@gmail.com>
2022-10-19 22:14:53 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alpha in lowercase
+ * print_alphabet_x10 - prints alpha 10 times
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
+	int ten;
 	char la;
 
-	for (la = 'a'; la <= 'z'; la++)
-		_putchar(la);
-
-	_putchar('\n');
+	for (ten = 0; ten<= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
 }
