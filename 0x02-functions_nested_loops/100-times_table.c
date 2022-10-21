99570e7a9696249d6b3e4bf825f393a2013da736
Sotayo Seun <sotayomatthews@gmail.com>
2022-10-20 19:26:25 -0700

    timmes table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,19 +1,11 @@
 #include "main.h"
-
 /**
- * print_times_table - print times table of the specified dimension
- * @n: Dimension of times table
- *
- * Return: Always 0
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
  */
-int main(void)
+void print_times_table(int n)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);
-    return (0);
-}
+
+	return (0);
+}i
